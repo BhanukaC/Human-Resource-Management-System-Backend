09bb5d8f94f7f0b2a98c88fa57fe9a910d596761
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2022-11-08 19:22:33 +0530

    Update hr.js

--- routes/hr.js
+++ routes/hr.js
@@ -6,48 +6,48 @@ const validAccess = require("../middleware/hrManager");
 const { employeeAddController, employeeUpdateController, employeeGetSingleController, employeeGetAllController, dependentAddController, dependentUpdateController, dependentGetSingleController, dependentGetAllController, otDataAddController, otDataUpdateController, otDataGetSingleController, otDataGetAllController, otAddController, otGetSingleController, otGetAllForEmployeeController, otGetAllController, advanceAddController, getAvailableAdvanceController, getAllAdvanceController, getSingleAdvanceController, getAllAdvanceForEmployeeController, addAttendanceController, viewSingleEmployeeAttendanceController, viewAllAttendanceController, calculateSalaryController, viewAllSalaryForEmployeeController, viewSingleSalaryController, viewAllSalaryController, getAllEmployeeEIDController } = require("../controllers/hrController");
 
 //employee
-router.post("/employee/add", [validateToken, validAccess], employeeAddController);
-router.put("/employee/update/:id", [validateToken, validAccess], employeeUpdateController);
-router.get("/employee/getSingle/:id", [validateToken, validAccess], employeeGetSingleController);
-router.get("/employee/getAll/", [validateToken, validAccess], employeeGetAllController);
-router.get("/employee/getAllEID/", [validateToken, validAccess], getAllEmployeeEIDController);
+router.post("/employee/add", employeeAddController);
+router.put("/employee/update/:id", employeeUpdateController);
+router.get("/employee/getSingle/:id", employeeGetSingleController);
+router.get("/employee/getAll/", employeeGetAllController);
+router.get("/employee/getAllEID/", getAllEmployeeEIDController);
 
 //dependent
-router.post("/dependent/add", [validateToken, validAccess], dependentAddController);
-router.put("/dependent/update/:id", [validateToken, validAccess], dependentUpdateController);
-router.get("/dependent/getSingle/:id", [validateToken, validAccess], dependentGetSingleController);
-router.get("/dependent/getAll/:id", [validateToken, validAccess], dependentGetAllController);
+router.post("/dependent/add", dependentAddController);
+router.put("/dependent/update/:id", dependentUpdateController);
+router.get("/dependent/getSingle/:id", dependentGetSingleController);
+router.get("/dependent/getAll/:id", dependentGetAllController);
 
 //OT Types
-router.post("/OtType/add", [validateToken, validAccess], otDataAddController);
-router.put("/otType/update/:id", [validateToken, validAccess], otDataUpdateController);
-router.get("/otType/getSingle/:id", [validateToken, validAccess], otDataGetSingleController);
-router.get("/otType/getAll/", [validateToken, validAccess], otDataGetAllController);
+router.post("/OtType/add", otDataAddController);
+router.put("/otType/update/:id", otDataUpdateController);
+router.get("/otType/getSingle/:id", otDataGetSingleController);
+router.get("/otType/getAll/", otDataGetAllController);
 
 //OT
-router.post("/Ot/add", [validateToken, validAccess], otAddController);
-router.get("/Ot/getSingle/:id", [validateToken, validAccess], otGetSingleController);
-router.get("/Ot/getAllForEmployee/:id", [validateToken, validAccess], otGetAllForEmployeeController);
-router.get("/Ot/getAll/", [validateToken, validAccess], otGetAllController);
+router.post("/Ot/add", otAddController);
+router.get("/Ot/getSingle/:id", otGetSingleController);
+router.get("/Ot/getAllForEmployee/:id", otGetAllForEmployeeController);
+router.get("/Ot/getAll/", otGetAllController);
 
 //advance
-router.post("/advance/add", [validateToken, validAccess], advanceAddController);
-router.get("/advance/balance/:id", [validateToken, validAccess], getAvailableAdvanceController);
-router.get("/advance/getAll/", [validateToken, validAccess], getAllAdvanceController);
-router.get("/advance/getSingle/:id", [validateToken, validAccess], getSingleAdvanceController);
-router.get("/advance/getAllForEmployee/:id", [validateToken, validAccess], getAllAdvanceForEmployeeController);
+router.post("/advance/add", advanceAddController);
+router.get("/advance/balance/:id", getAvailableAdvanceController);
+router.get("/advance/getAll/", getAllAdvanceController);
+router.get("/advance/getSingle/:id", getSingleAdvanceController);
+router.get("/advance/getAllForEmployee/:id", getAllAdvanceForEmployeeController);
 
 //attendance
-router.post("/attendance/add", [validateToken, validAccess], addAttendanceController);
-router.get("/attendance/getAll/", [validateToken, validAccess], viewAllAttendanceController);
-router.get("/attendance/getSingle/:id", [validateToken, validAccess], viewSingleEmployeeAttendanceController);
+router.post("/attendance/add", addAttendanceController);
+router.get("/attendance/getAll/", viewAllAttendanceController);
+router.get("/attendance/getSingle/:id", viewSingleEmployeeAttendanceController);
 
 
 //salary
-router.get("/salary/calculate", [validateToken, validAccess], calculateSalaryController);
-router.get("/salary/getAll", [validateToken, validAccess], viewAllSalaryController);
-router.get("/salary/getSingle/:id", [validateToken, validAccess], viewSingleSalaryController);
-router.get("/salary/getAllForEmployee/:id", [validateToken, validAccess], viewAllSalaryForEmployeeController);
+router.get("/salary/calculate", calculateSalaryController);
+router.get("/salary/getAll", viewAllSalaryController);
+router.get("/salary/getSingle/:id", viewSingleSalaryController);
+router.get("/salary/getAllForEmployee/:id", viewAllSalaryForEmployeeController);
 
 
 
