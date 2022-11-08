8ce285bd012f1b542c563ebdfc8baa750f129512
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2022-11-08 16:43:38 +0530

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    user: process.env.USER,
-    host: process.env.HOST,
-    port: process.env.PORTDB,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    user: admin,
+    host: hr.cu11gcggg3b7.us-east-1.rds.amazonaws.com,
+    port: 3306,
+    password: adminPassword,
+    database: hr
 });
 
 module.exports = db;
